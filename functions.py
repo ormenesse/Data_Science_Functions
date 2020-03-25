052ba732041dfd7f79d8e3f9aea94f4374693d05
Vinícius Ormenesse <viniciusormenesse@gmail.com>
2020-03-25 14:16:54 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -196,3 +196,9 @@ def kfoldcv(indices, k = 5, seed = 4242):
                 trainz = np.concatenate((trainz,subset),axis=0)
         train.append(list(trainz))
     return train,test
+
+def display_side_by_side(*args):
+    html_str=''
+    for df in args:
+        html_str+=df.to_html()
+    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
